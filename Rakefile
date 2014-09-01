c0d5e18a7fab9bf96c7109bb4b41c3f271797de5
jetBeaver <jetBeaver@gmail.com>
2014-09-02 00:04:31 +0900

    impl. config file reading

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 require 'rake/clean'
 
 CC = "gcc"
+cflags = "-std=c99 -Wall"
 SRCS = FileList["*.c"]
 OBJS = SRCS.ext('o')
 
@@ -9,12 +10,17 @@ CLOBBER.include("monitctl")
 
 task :default => :monitctl
 
+task :debug do
+  cflags += " -DDEBUG -g"
+  Rake::Task['monitctl'].invoke
+end
+
 desc "build monitctl"
 file :monitctl => OBJS do |t|
-  sh "#{CC} -o #{t.name} #{t.prerequisites.join(' ')}"
+  sh "#{CC} #{cflags} -o #{t.name} #{t.prerequisites.join(' ')}"
 end
 
 desc "complile all sources"
 rule '.o' => ['.c'] do |t|
-  sh "#{CC} -c #{t.source} -o #{t.name}"
+  sh "#{CC} #{cflags} -c #{t.source} -o #{t.name}"
 end
--- monitctl.c
+++ monitctl.c
@@ -5,51 +5,57 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 #define NO_CHANGE -1
 
+#define BUF_SIZE 256
+#define MAX_ACTION 32
+
 #ifndef CONFIG_FILE
-	#define CONFIG_FILE "/etc/monitctl"
+    #define CONFIG_FILE "/etc/monitctl.conf"
 #endif
 
 typedef struct {
-	char *config_file;
-	char *group_name;
-	char *action;
+    char *config_file;
+    char *group_name;
+    char *action;
+    char *target;
 } Options;
 
 typedef struct {
-	char *monit_path;
-	char **allow_actions;
-	int  action_num;
+    char *monit_path;
+    char **allow_actions;
+    int  action_num;
 } Configs;
 
 void print_usege(void)
 {
-	fprintf(stdout, "Usage: monitctl [OPTION]... MONIT_ACTION\n"
-					" monitctl calls optional action arguments of monit.\n"
-					" Options are as follows:\n"
-					"  -g name          Set group name for start, stop, restart, monitor and unmonitor\n"
-					"  -c config_file   Use this monitctl control file\n"
-	);
+    fprintf(stdout, "Usage: monitctl [OPTION]... MONIT_ACTION\n"
+                    " monitctl calls optional action arguments of monit.\n"
+                    " Options are as follows:\n"
+                    "  -g name          Set group name for start, stop, restart, monitor and unmonitor\n"
+                    "  -c config_file   Use this monitctl control file (default:" CONFIG_FILE ")\n"
+                    );
 }
 
 Options* get_options(int argc, char *argv[])
 {
-	Options *opts;
-	int opt;
+    Options *opts;
+    int opt;
 
-	opts = (Options *)malloc(sizeof(Options));
-	opts->config_file = CONFIG_FILE;
-	opts->group_name  = NULL;
-	opts->action      = NULL;
+    opts = (Options *)malloc(sizeof(Options));
+    opts->config_file = CONFIG_FILE;
+    opts->group_name  = NULL;
+    opts->action      = NULL;
+    opts->target      = NULL;
 
-	while ((opt = getopt(argc, argv, "c:g:")) != -1) {
+    while ((opt = getopt(argc, argv, "c:g:")) != -1) {
         switch (opt) {
-        case 'g':
-        	opts->group_name = optarg;
+            case 'g':
+            opts->group_name = optarg;
             break;
-        case 'c':
+            case 'c':
             opts->config_file = optarg;
             break;
         default: /* '?' or ':' */
@@ -62,118 +68,177 @@ Options* get_options(int argc, char *argv[])
     }
 
     opts->action = argv[optind];
+    if(optind + 1 < argc) {
+        opts->target = argv[optind + 1];
+    }
 
     return opts;
 }
 
+bool check_config_file_permission(Options *opts)
+{
+    int FILE_MODE_MASK = S_IRWXU | S_IRWXG | S_IRWXO;
+    int RW_ONLY_OWNER = S_IRUSR | S_IWUSR;
+    struct stat info;
+
+    if(!stat(opts->config_file, &info)) {
+        fprintf(stderr, "get config file stat error\n");
+        return false;
+    }
+
+    if(S_ISREG(info.st_mode) &&                            // Regular file and,
+       (info.st_mode & FILE_MODE_MASK) == RW_ONLY_OWNER && // File mode is 0600 and,
+       info.st_uid == getuid()                             // real uid(after reset effective uid) is same.
+       ) {
+        return true;
+    }
+
+    fprintf(stderr, "Config file(%s) file mode must be 0600 and owner must be effective uid\n", opts->config_file);
+    return false;
+}
+
 Configs* get_configs(Options *opts)
 {
-	Configs *configs;
-	FILE *fp;
-	char line[256], key[256], value[256];
-	char *action;
-	int index;
+    Configs *configs;
+    FILE *fp;
+    char line[BUF_SIZE], key[BUF_SIZE], value[BUF_SIZE];
+    char *action;
+    int index;
 
-	configs = (Configs *)malloc(sizeof(Configs));
-	configs->monit_path = "";
-	configs->allow_actions = (char **)malloc(sizeof(char *) * 32);
-	configs->action_num = 0;
+    configs = (Configs *)malloc(sizeof(Configs));
+    configs->monit_path = "";
+    configs->allow_actions = (char **)malloc(sizeof(char *) * MAX_ACTION);
+    configs->action_num = 0;
 
-	fp = fopen(opts->config_file, "r");
-	if(fp == NULL) {
-	  fprintf(stderr, "config file open error (file:%s)", opts->config_file);
-	}
+    fp = fopen(opts->config_file, "r");
+    if(fp == NULL) {
+        fprintf(stderr, "config file open error (file:%s)\n", opts->config_file);
+        return NULL;
+    }
 
-	while(fgets(line, sizeof(line), fp) != NULL) {
-	  if(line[0] == '#') {
-	    continue;
-	  }
+    while(fgets(line, sizeof(line), fp) != NULL) {
+        if(line[0] == '#') {
+            continue;
+        }
 
-	  sscanf(line, "%255s%255s", key, value);
-	  if(strcmp(key, "monit_path") == 0) {
-	    configs->monit_path = strdup(value);
-	  }
-	  else if(strcmp(key, "allow_actions") == 0) {
-	    for(action = strtok(value, ","); action; action = strtok(NULL, ",")) {
-	      index = configs->action_num;
-	      configs->allow_actions[index] = strdup(action);
-	      configs->action_num++;
-	    }
-	  }
-	}
+        sscanf(line, "%255s%255s", key, value);
+        if(strcmp(key, "monit_path") == 0) {
+            configs->monit_path = strdup(value);
+        }
+        else if(strcmp(key, "allow_actions") == 0) {
+            action = strtok(value, ",");
+            while(action && configs->action_num < MAX_ACTION) {
+                index = configs->action_num;
+                configs->allow_actions[index] = strdup(action);
+                configs->action_num++;
 
-	//configs->monit_path = "/usr/bin/monit";
+                action = strtok(NULL, ",");
+            }
+        }
+    }
 
-	return configs;
+#ifdef DEBUG
+    fprintf(stderr, "[DEBUG] monit_path=%s\n", configs->monit_path);
+    for(int i = 0; i < configs->action_num; i++) {
+        fprintf(stderr, "[DEBUG] allow_action%02d=%s\n", i, configs->allow_actions[i]);
+    }
+#endif
+
+    return configs;
+}
+
+bool check_allow_actions(Options *opts, Configs *configs)
+{
+    int i;
+
+    for(i = 0; i < configs->action_num; i++) {
+        if(strcmp(opts->action, configs->allow_actions[i]) == 0)
+            return true;
+    }
+
+    fprintf(stderr, "action '%s' is forbidden\n", opts->action);
+
+    return false;
 }
 
 bool reset_real_uid(void)
 {
-	uid_t ruid, euid, suid;
-	gid_t rgid, egid, sgid;
-	int ret_getresuid, ret_getresgid;
-	int ret_setreuid, ret_setregid;
+    uid_t ruid, euid, suid;
+    gid_t rgid, egid, sgid;
+    int ret_getresuid, ret_getresgid;
+    int ret_setreuid, ret_setregid;
 
-	// get effective uid and effective gid.
-	ret_getresuid = getresuid(&ruid, &euid, &suid);
-	ret_getresgid = getresgid(&rgid, &egid, &sgid);
+    // get effective uid and effective gid.
+    ret_getresuid = getresuid(&ruid, &euid, &suid);
+    ret_getresgid = getresgid(&rgid, &egid, &sgid);
 
-	if(ret_getresuid != 0 || ret_getresgid != 0) {
-		fprintf(stderr, "getresuid/getresgid error\n");
-		return false;
-	}
+    if(ret_getresuid != 0 || ret_getresgid != 0) {
+        fprintf(stderr, "getresuid/getresgid error\n");
+        return false;
+    }
 
-	// set effective uid to real uid.
-	// set effective gid to real gid.
-	ret_setreuid = setreuid(euid, NO_CHANGE);
-	ret_setregid = setregid(egid, NO_CHANGE);
+    // set effective uid to real uid.
+    // set effective gid to real gid.
+    ret_setreuid = setreuid(euid, NO_CHANGE);
+    ret_setregid = setregid(egid, NO_CHANGE);
 
-	if(ret_setreuid != 0 || ret_setregid != 0) {
-		fprintf(stderr, "setreuid/setregid error\n");
-		return false;
-	}
+    if(ret_setreuid != 0 || ret_setregid != 0) {
+        fprintf(stderr, "setreuid/setregid error\n");
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 void invoke_monit(Options *opts, Configs *configs)
 {
-	char *args[4];
-	int index = 0;
+    char *args[5];
+    int index = 0;
 
-	args[index++] = configs->monit_path;
-	if(opts->group_name) {
-		args[index++] = "-g";
-		args[index++] = opts->group_name;
-	}
-	args[index++] = opts->action;
-	args[index++] = NULL;
+    args[index++] = configs->monit_path;
+    if(opts->group_name) {
+        args[index++] = "-g";
+        args[index++] = opts->group_name;
+    }
+    args[index++] = opts->action;
+    if(opts->target) {
+        args[index++] = opts->target;
+    }
+    args[index++] = NULL;
 
-	execv(configs->monit_path, args);
+    execv(configs->monit_path, args);
 }
 
 int main(int argc, char *argv[])
 {
-	Options *opts;
-	Configs *configs;
+    Options *opts;
+    Configs *configs;
 
-	opts = get_options(argc, argv);
-	if(opts == NULL) {
-		print_usege();
-		exit(EXIT_FAILURE);
-	}
+    opts = get_options(argc, argv);
+    if(opts == NULL) {
+        print_usege();
+        exit(EXIT_FAILURE);
+    }
 
-	if(reset_real_uid() == false) {
-		exit(EXIT_FAILURE);
-	}
+    if(reset_real_uid() == false) {
+        exit(EXIT_FAILURE);
+    }
 
-	configs = get_configs(opts);
-	if(configs == NULL) {
-		exit(EXIT_FAILURE);
-	}
+    if(check_config_file_permission(opts) == false) {
+        exit(EXIT_FAILURE);
+    }
 
-	invoke_monit(opts, configs);
+    configs = get_configs(opts);
+    if(configs == NULL) {
+        exit(EXIT_FAILURE);
+    }
 
-	fprintf(stderr, "monit execution error\n");
-	exit(EXIT_FAILURE);
+    if(check_allow_actions(opts, configs) == false) {
+        exit(EXIT_FAILURE);
+    }
+
+    invoke_monit(opts, configs);
+
+    fprintf(stderr, "monit execution error\n");
+    exit(EXIT_FAILURE);
 }
